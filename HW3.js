651767439b4f9448f54566aeeb66cd4464524202
yanhvo <34598655+yanhvo@users.noreply.github.com>
2020-01-20 15:40:49 +0300

    Bugs fixed after review

--- HW3.js
+++ HW3.js
@@ -1,17 +1,20 @@
 // task 1
-function splitAndMerge(sentence, sep){
+function splitAndMerge(sentence, sep) {
   var arr_words = sentence.split(' ');
-  var arr_reswords = arr_words.map(function(one_word){ return one_word.split('').join(sep);});
-  return(arr_reswords.join(' '));
+  var arr_reswords = [];
+  for (var i = 0; i < arr_words.length; i++) {
+    arr_reswords[i] = arr_words[i].split('').join(sep);
+  }
+  return (arr_reswords.join(' '));
 }
 //console.log(splitAndMerge("My name is John",":"));
 
 //-----------------------
 
 // task 2
-function convert(hash){
+function convert(hash) {
   var arr = [];
-  for(var k in hash){
+  for (var k in hash) {
     arr.push([k, hash[k]]);
   }
   return arr;
@@ -23,11 +26,13 @@ function convert(hash){
 //-----------------------
 
 // task 3
-function toCamelCase(delim_words){
+function toCamelCase(delim_words) {
   var arr_words = delim_words.split(/\-|\_/);
   var first_word = arr_words[0];
   arr_words.shift(0);
-  arr_words = arr_words.map(function(one_word){ return one_word[0].toUpperCase() + one_word.substring(1);});
+  for (var i = 0; i < arr_words.length; i++) {
+    arr_words[i] = arr_words[i][0].toUpperCase() + arr_words[i].substring(1);
+  }
   arr_words.unshift(first_word);
   return arr_words.join('');
 }
@@ -37,9 +42,12 @@ function toCamelCase(delim_words){
 //-----------------------
 
 // task 4
-function toReverse(sentence){
+function toReverse(sentence) {
   var arr_words = sentence.split(' ');
-  var res = arr_words.map(function(one_word){ return one_word.split('').reverse().join('');});
+  var res = [];
+  for (var i = 0; i < arr_words.length; i++) {
+    res[i] = arr_words[i].split('').reverse().join('');
+  }
   return res.join(' ');
 }
 
@@ -48,32 +56,36 @@ function toReverse(sentence){
 //-----------------------
 
 // task 5
-function stringExpansion(string){
-  var arr_char=0;
+function stringExpansion(string) {
+  var arr_char = 0;
   var res = '';
   var temp = 1;
-  for(i=0; i<string.length; i++){
-    if(!isNaN(parseInt(string[i], 10)) && i+1<=string.length && isNaN(parseInt(string[i+1], 10))){
+  for (i = 0; i < string.length; i++) {
+    if (!isNaN(parseInt(string[i], 10)) && !isNaN(parseInt(string[i + 1], 10)) ) {
+      continue;
+    } else if (!isNaN(parseInt(string[i], 10))) {
       temp = parseInt(string[i], 10);
       continue;
     }
-    for(j=1; j<=temp; j++){
-      res= res + string[i];
+    for (j = 1; j <= temp; j++) {
+      res += string[i];
     }
   }
   return res;
 }
 
-//console.log(stringExpansion('dchjj'));
+//console.log(stringExpansion('3Fd2a5d2f')); //FFFdddaadddddff
+//console.log(stringExpansion('3d342f2a')); //dddffaa
+//console.log(stringExpansion('dchjj')); //dchjj
 
 //-----------------------
 
 // task 6
-function largest(){
+function largest() {
   return Math.max.apply(null, arguments);
 }
 
-function smallest(){
+function smallest() {
   return Math.min.apply(null, arguments);
 }
 
@@ -83,9 +95,9 @@ function smallest(){
 //-----------------------
 
 //task7
-function transform(arr){
-  return arr.map(function(item){
-    return function(){
+function transform(arr) {
+  return arr.map(function(item) {
+    return function() {
       return item;
     }
   });
@@ -99,11 +111,11 @@ function transform(arr){
 //-----------------------
 
 //task8
-function sum(...args){
+function sum(...args) {
   var arr_char = args;
 
-  function summ(arr_char){
-    if (arr_char.length === 0){
+  function summ(arr_char) {
+    if (arr_char.length === 0) {
             return 0;
           } else {
             return arr_char[0] + summ(arr_char.slice(1));
@@ -117,12 +129,28 @@ function sum(...args){
 //-----------------------
 
 //task9
-function countDown(num){
-  if (num<0){
+function countDown(num) {
+  if (num < 0) {
     return 0;
-  } else{
+  } else {
   console.log(num);
-  return setTimeout(countDown, 1000, num-1);}
+  return setTimeout(countDown, 1000, num - 1);
+  }
 }
 
 //countDown(3); // 3 2 1 0
+
+//-----------------------
+
+//task10
+Function.prototype.myBind = function(context) {
+    var this_function = this;
+    return function() {
+        var func_args = Array.prototype.slice.call(arguments);
+        return this_function.apply(context, func_args);
+    }
+}
+
+//function addPropToNumber(number) { return this.prop + number; }
+//var bound = addPropToNumber.myBind({ prop: 9 });
+//console.log(bound(1)); // 10
